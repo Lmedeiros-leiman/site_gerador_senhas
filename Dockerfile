1c003512e89987158bfc2bb5da2facbed4125e27
Lmedeiros-leiman <lmedeiros045@gmail.com>
2023-11-01 19:25:07 -0300

    commit inicial

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["learningNETwebApp/learningNETwebApp.csproj", "learningNETwebApp/"]
+RUN dotnet restore "learningNETwebApp/learningNETwebApp.csproj"
+COPY . .
+WORKDIR "/src/learningNETwebApp"
+RUN dotnet build "learningNETwebApp.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "learningNETwebApp.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "learningNETwebApp.dll"]
---
+++ wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const botao = document.getElementById("input");
+const blocosenha = document.getElementById("output");
+
+botao.addEventListener("click", () => { gerarsenha() } );
+
+
+function gerarsenha() 
+{
+    alert("eae");
+    
+    return;
+    
+    const caracteres_especiais = "_=-/\'!@#$%¨&*(){}^?:><";
+    const letras = "abcdefghijklmnopqrstuvwxyz";
+    const numeros = "1234567890";
+    let novasenha = "";
+
+    const tamanhosenha = document.getElementById("tamanho").value;
+    
+    while (novasenha.length < tamanhosenha) 
+    {
+        let numero = Math.random() * 100;
+        
+        switch (numero)
+        {
+            case numero < 33:
+                novasenha = novasenha + letras[Math.floor(Math.random() * letras.length)];
+                break;
+                
+            case numero >= 33 && numero <= 66:
+                novasenha = novasenha + caracteres_especiais[Math.floor(Math.random() * caracteres_especiais.length)];
+                break;
+                
+            case numero > 66:
+                novasenha = novasenha + numeros[Math.floor(Math.random() * numeros.length)];
+                break;
+        }
+    }
+    
+    
+    blocosenha.innerHTML = novasenha;
+    
+}
